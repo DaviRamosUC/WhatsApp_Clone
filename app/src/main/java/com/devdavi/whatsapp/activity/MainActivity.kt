6bfc74895d95fba561fc91b6862a6057fdcc6cac
Davi Ramos Lima <davi.lima@ucsal.edu.br>
2022-03-01 15:11:12 -0300

    Adicionando menu e opção de sair

--- app/src/main/java/com/devdavi/whatsapp/activity/MainActivity.kt
+++ app/src/main/java/com/devdavi/whatsapp/activity/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.devdavi.whatsapp.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.devdavi.whatsapp.R
 import com.devdavi.whatsapp.databinding.ActivityMainBinding
+import com.devdavi.whatsapp.utils.FirebaseConfig
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var autenticacao = FirebaseConfig.autenticacao
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,4 +22,33 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar.toolbarPrincipal)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menuSair -> {
+                deslogarUsuario()
+                startActivity(Intent(this, LoginActivity::class.java))
+                finish()
+            }
+
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun deslogarUsuario() {
+        try {
+            autenticacao.signOut()
+        } catch (e: Exception) {
+            Toast.makeText(this, "Erro ao deslogar o usuário", Toast.LENGTH_LONG)
+                .show()
+            e.printStackTrace()
+        }
+    }
+
 }
