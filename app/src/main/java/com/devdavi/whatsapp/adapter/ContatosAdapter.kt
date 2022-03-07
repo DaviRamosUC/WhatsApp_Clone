548cc982564557fa3d529ad6c61703f895564735
Davi Ramos Lima <davi.lima@ucsal.edu.br>
2022-03-07 19:28:58 -0300

    Listando contatos

---
+++ app/src/main/java/com/devdavi/whatsapp/adapter/ContatosAdapter.kt
@@ -0,0 +1,49 @@
+package com.devdavi.whatsapp.adapter
+
+import android.content.Context
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.devdavi.whatsapp.R
+import com.devdavi.whatsapp.model.Usuario
+import com.google.android.material.imageview.ShapeableImageView
+
+class ContatosAdapter(listaContatos: ArrayList<Usuario>, c: Context?) :
+    RecyclerView.Adapter<ContatosAdapter.MyViewHolder>() {
+
+    private var listaContatos: List<Usuario> = listaContatos
+    private var context: Context? = c
+
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val foto: ShapeableImageView = itemView.findViewById(R.id.imageViewFotoContato)
+        val nome: TextView = itemView.findViewById(R.id.textNomeContato)
+        val email: TextView = itemView.findViewById(R.id.textEmailContato)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val itemLista =
+            LayoutInflater.from(parent.context).inflate(R.layout.adapter_contatos, parent, false)
+        return MyViewHolder(itemLista)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val usuario: Usuario = listaContatos.get(position)
+        holder.nome.text = usuario.nome
+        holder.email.text = usuario.email
+
+        if (usuario.foto != null) {
+            val uri = Uri.parse(usuario.foto)
+            Glide.with(context!!).load(uri).into(holder.foto)
+        } else {
+            holder.foto.setImageResource(R.drawable.padrao)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return listaContatos.size
+    }
+}
--- app/src/main/java/com/devdavi/whatsapp/fragments/ContatosFragment.kt
+++ app/src/main/java/com/devdavi/whatsapp/fragments/ContatosFragment.kt
@@ -5,9 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.devdavi.whatsapp.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.devdavi.whatsapp.adapter.ContatosAdapter
+import com.devdavi.whatsapp.databinding.FragmentContatosBinding
+import com.devdavi.whatsapp.model.Usuario
+import com.devdavi.whatsapp.utils.FirebaseConfig
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
 
 class ContatosFragment : Fragment() {
+
+    private lateinit var binding: FragmentContatosBinding
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: ContatosAdapter
+    private val listaContatos: ArrayList<Usuario> = ArrayList()
+    private lateinit var usuariosRef: DatabaseReference
+    private lateinit var valueEventListener: ValueEventListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -15,8 +32,47 @@ class ContatosFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_contatos, container, false)
+    ): View {
+        binding = FragmentContatosBinding.inflate(inflater, container, false)
+        usuariosRef = FirebaseConfig.database.reference.child("usuarios")
+        recyclerView = binding.recyclerViewListaContatos
+        //Configurar o adapter
+        adapter = ContatosAdapter(listaContatos, activity)
+
+        //Configurar o recyclerView
+        val layoutManager = LinearLayoutManager(activity)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.setHasFixedSize(true)
+        recyclerView.adapter = adapter
+
+        return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        recuperarContatos()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        usuariosRef.removeEventListener(valueEventListener)
+    }
+
+    fun recuperarContatos() {
+        valueEventListener = usuariosRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for (dados in snapshot.children) {
+                    val usuario = dados.getValue(Usuario::class.java)
+                    listaContatos.add(usuario!!)
+                }
+                adapter.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
     }
 
 }
