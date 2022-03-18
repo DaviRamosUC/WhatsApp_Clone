b4ad1768bf57576a753fd5a8f407a203c8561ae2
Davi Ramos Lima <davi.lima@ucsal.edu.br>
2022-03-17 21:48:03 -0300

    Criando interface de cadastro de grupo

--- app/src/main/java/com/devdavi/whatsapp/activity/CadastroGrupoActivity.kt
+++ app/src/main/java/com/devdavi/whatsapp/activity/CadastroGrupoActivity.kt
@@ -1,20 +1,30 @@
 package com.devdavi.whatsapp.activity
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.devdavi.whatsapp.adapter.GrupoSelecionadoAdapter
 import com.devdavi.whatsapp.databinding.ActivityCadastroGrupoBinding
 import com.devdavi.whatsapp.model.Usuario
+import com.devdavi.whatsapp.utils.RecyclerItemClickListener
 import com.google.android.material.snackbar.Snackbar
 
 class CadastroGrupoActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityCadastroGrupoBinding
     private val listaMembrosSelecionados = ArrayList<Usuario>()
+    private lateinit var grupoSelecionadoAdapter: GrupoSelecionadoAdapter
+    private lateinit var recyclerMembrosSelecionados: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCadastroGrupoBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.toolbar.title = "Novo Grupo"
+        binding.toolbar.subtitle = "Defina o nome"
         setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
@@ -23,11 +33,25 @@ class CadastroGrupoActivity : AppCompatActivity() {
                 .setAction("Action", null).show()
         }
 
+        recyclerMembrosSelecionados = binding.contentCadastroGrupo.recyclerMembrosGrupo
+
         //Recuperar lista de membros passada
         if (intent.extras != null) {
             val membros = intent.extras!!.getSerializable("membros") as List<Usuario>
             listaMembrosSelecionados.addAll(membros)
-            binding.contentCadastroGrupo.textTotal.text = listaMembrosSelecionados.size.toString()
+            binding.contentCadastroGrupo.textTotalParticipantes.text =
+                "Participantes: ${listaMembrosSelecionados.size}"
         }
+
+        //Configurar o recyclerview
+        //Configurando recyclerview para os membros selecionados
+        grupoSelecionadoAdapter =
+            GrupoSelecionadoAdapter(listaMembrosSelecionados, applicationContext)
+
+        val layoutManagerHorizontal =
+            LinearLayoutManager(applicationContext, LinearLayoutManager.HORIZONTAL, false)
+        recyclerMembrosSelecionados.layoutManager = layoutManagerHorizontal
+        recyclerMembrosSelecionados.setHasFixedSize(true)
+        recyclerMembrosSelecionados.adapter = grupoSelecionadoAdapter
     }
 }
